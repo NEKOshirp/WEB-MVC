34bce0c70c0b8c5e788626824230cbe8ccc12999
NEKOshirp <124141538+NEKOshirp@users.noreply.github.com>
2024-04-10 22:51:22 +0700

    s
    as

---
+++ TrainingFPTCo/Controllers/TrainingStaffController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TrainingFPTCo.Controllers
+{
+    public class TrainingStaffController : Controller
+    {
+        public IActionResult Index()
+        {
+            if (string.IsNullOrEmpty(HttpContext.Session.GetString("SessionUserId")))
+            {
+                return RedirectToAction(nameof(LoginController.Index), "Login");
+            }
+            return View();
+        }
+    }
+}
---
+++ TrainingFPTCo/Models/AssignCourseViewModel.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System.Collections.Generic;
+
+namespace TrainingFPTCo.Models
+{
+    public class AssignCourseViewModel
+    {
+        public int CourseId { get; set; }
+        public string CourseName { get; set; }
+        public int RoleId { get; set; }
+        public List<SelectListItem> TrainerList { get; set; }
+    }
+}
